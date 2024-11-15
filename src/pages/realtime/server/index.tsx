d55bbfebab36e5ac4be8add60ef694df520ad19d
dearlin <dearlin2024@qq.com>
2024-11-15 13:11:43 +0800

    fix:打包问题处理

--- src/pages/realtime/server/index.tsx
+++ src/pages/realtime/server/index.tsx
@@ -1,32 +1,10 @@
-import { healthApi } from '@/api/request';
-import { theme } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import HouyiServer from './houyi_server';
 import styles from './index.module.scss';
 import './index.scss';
 import RabbimtServer from './rabbit_server';
-
 export interface LoginProps { }
-const { useToken } = theme
-
-let timer: NodeJS.Timeout | null = null
 const Login: React.FC<LoginProps> = () => {
-    const { token } = useToken()
-    const [version, setVersion] = useState('version')
-
-    const getVersion = () => {
-        if (timer) {
-            clearTimeout(timer)
-        }
-        timer = setTimeout(() => {
-            healthApi().then((res) => {
-                setVersion(res.version)
-            })
-        }, 300)
-    }
-    useEffect(() => {
-        getVersion()
-    }, [])
     return (
         <div className={styles.box}>
             <HouyiServer></HouyiServer>
