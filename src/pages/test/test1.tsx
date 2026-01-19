58690fe1b111ef4440d4e77508cb2151e81efc21
wangli <2390197194@qq.com>
2026-01-19 13:40:35 +0800

    refactor: clean up styles and update test component
    - Removed unused index.css file from the test app.
    - Updated index.html to comment out the old stylesheet link.
    - Modified main.tsx to remove the import of the deleted index.css.
    - Changed the text color in test1.tsx from red to blue for better visibility.
    - Enhanced index.css by adding a source directive for improved style management.

--- src/pages/test/test1.tsx
+++ src/pages/test/test1.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 export default function Test1() {
-  return <div className='text-red-500 test1 font-bold underline'>Test1</div>
+  return <div className='text-blue-500 test1 font-bold underline'>Test1</div>
 }
